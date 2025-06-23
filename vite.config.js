3342968b22ab3929b4f71f3cd19309ac4419d371
Jonathan V Tran <jtran@tulane.edu>
2025-06-23 12:57:23 -0500

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === 'build' ? '/<YOUR_REPO_NAME>/' : '/';
+  const base = command === 'build' ? '/swim-meet-app/' : '/';
 
   return {
     base: base,
